67da058a402b81cd58d646fca52a72abf51cfb06
amitkumarisdevelopeer <ak14prakas@gmail.com>
2024-04-24 01:24:14 +0530

    new page is added

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ var page2=document.querySelector("#page2")
 var cross=document.querySelector("#cross")
 var menu=document.querySelector("#menu")
 var page11=document.querySelector("#page11")
-
+var on1=document.querySelector("#three1 #on1")
+var off1=document.querySelector("#three1 #off1")
 menu.addEventListener("click",function(){
     menu.style.display="none"
     cross.style.display="block"
@@ -52,3 +53,49 @@ off.addEventListener("click",function(){
     on.style.opacity="1"
 
 })
+
+on1.addEventListener("click",function(){
+    page1.style.backgroundColor="black"
+    left.style.backgroundColor="black"
+    right.style.backgroundColor="black"
+    page2.style.backgroundColor="black"
+    h1.style.color="white"
+    p.style.color="white"
+    nav.style.backgroundColor="#64D6D2"
+
+    // text.style.color="red"
+    off.style.opacity="1"
+    on.style.opacity="0"
+
+})
+
+off1.addEventListener("click",function(){
+    page1.style.backgroundColor="white"
+    left.style.backgroundColor="#F5F5F5"
+    right.style.backgroundColor="#F5F5F5"
+    page2.style.backgroundColor="white"
+    h1.style.color="#1B1B1B"
+    p.style.color="#1B1B1B"
+    text.style.color="black"
+    nav.style.backgroundColor="#D9E5E7"
+    off.style.opacity="0"
+    on.style.opacity="1"
+
+})
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
